f94318b42fe10eff8dd50c1bf18506d50f2d077a
Alizhan Manap <254732@astanait.edu.kz>
2026-01-16 08:56:34 +0500

    Alizhan Manap push Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.sql.*;
 
 public class Main {
+    public static void main(String[] args) {
+        String connectionUrl = "jdbc:postgresql://localhost:5432/SimpleDB";
+        String user = "postgres";
+        String pass = "alizhan1234";
+
+        // Ресурсы в скобках try закроются автоматически
+        try (Connection con = DriverManager.getConnection(connectionUrl, user, pass);
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM users")) {
+
+            System.out.println("Подключение успешно! Запрашиваю данные...");
+            boolean hasData = false;
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + " "
+                        + rs.getString("name") + " "
+                        + rs.getString("surname"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
